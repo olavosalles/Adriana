7649a6478d674909152a3291cc0a5918c3af37c6
olavosalles <olavosalles123@gmail.com>
2026-08-13 19:39:37 -0300

    Add files via upload

---
+++ Exercicios Java/Atividades/src/Temperatura.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Temperatura {
+    public static void main(String[] args){
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Qual a temperatura em graus celsius?");
+        double temp=s.nextDouble();
+        if (temp > 18){
+            System.out.println("A temperatura em São José dos Campos está agradável!");
+        }
+        else{
+            System.out.println("Está frio em São José dos Campos!");
+        }
+
+    }
+
+}
